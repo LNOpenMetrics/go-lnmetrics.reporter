e9d34f5b1b77d7032d8dee50ed570c31457839c3
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-06-19 19:59:34 +0200

    Fixed plugin implementation.
    Adding the Call method and New method.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- internal/plugin/diagnostic.go
+++ internal/plugin/diagnostic.go
@@ -5,16 +5,29 @@ import (
 	"github.com/niftynei/glightning/jrpc2"
 )
 
-type DiagnosticRpcMethod struct{}
+type DiagnosticRpcMethod struct {
+	// all the error that will happen durng the metrics
+	errors   map[int][]string
+	memoryDB *MemoryDB
+}
 
 func (rpc *DiagnosticRpcMethod) Name() string {
 	return "diagnostic"
 }
 
+func NewMetricPlugin() *DiagnosticRpcMethod {
+	return &DiagnosticRpcMethod{errors: make(map[int][]string),
+		memoryDB: &MemoryDB{nodeId: "", metrics: make(map[int]*Metric)}}
+}
+
 func (rpc *DiagnosticRpcMethod) New() interface{} {
-	return &DiagnosticRpcMethod{}
+	return NewMetricPlugin()
 }
 
 func (rpc *DiagnosticRpcMethod) Call() (jrpc2.Result, error) {
+	return fmt.Sprintf("Here will be the diagnostic"), nil
+}
+
+func (rpc *DiagnosticRpcMethod) UpdateWithMessage(msg *Msg) (jrpc2.Result, error) {
 	return fmt.Sprintf("Here will be the diagnostic"), nil
 }
--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -18,6 +18,7 @@ func (plugin *MetricsPlugin) HendlerRPCMessage(event *glightning.RpcCommandEvent
 	}
 	switch method.(type) {
 	case glightning.CloseRequest:
+		// Share to all the metrics, so we need a global method that iterate over the metrics map
 		log.GetInstance().Debug("Close command received")
 	default:
 		log.GetInstance().Debug("The node is up and runnning update the info")
@@ -26,7 +27,8 @@ func (plugin *MetricsPlugin) HendlerRPCMessage(event *glightning.RpcCommandEvent
 }
 
 func (plugin *MetricsPlugin) RegisterMethods() {
-	rpcMethod := glightning.NewRpcMethod(&DiagnosticRpcMethod{}, "Show diagnostic node")
+	method := NewMetricPlugin()
+	rpcMethod := glightning.NewRpcMethod(method, "Show diagnostic node")
 	rpcMethod.LongDesc = "Show the diagnostic data of the lightning network node"
 	rpcMethod.Category = "metrics"
 	plugin.Plugin.RegisterMethod(rpcMethod)
