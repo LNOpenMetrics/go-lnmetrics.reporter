bdd1543cc5ff4278c82a5f7c73f4c456294bad7f
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-11-18 16:04:02 +0100

    update cln4go dependences
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ pkg/trace/tracer.go
@@ -0,0 +1,20 @@
+package trace
+
+import (
+	"github.com/LNOpenMetrics/lnmetrics.utils/log"
+	"github.com/vincenzopalazzo/cln4go/comm/tracer"
+)
+
+type Tracer struct{}
+
+func (self *Tracer) Log(lebel tracer.TracerLevel, msg string) {}
+
+func (self *Tracer) Logf(level tracer.TracerLevel, msg string, args ...any) {}
+
+func (self *Tracer) Info(msg string) {
+	log.GetInstance().Info(msg)
+}
+
+func (self *Tracer) Infof(msg string, args ...any) {
+	log.GetInstance().Infof(msg, args...)
+}
